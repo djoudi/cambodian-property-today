11df9d9b5927573d168139ddbb38594cb79f7407
chen seng <chen.webdev@gmail.com>
2010-12-06 10:32:25 +0700

    change style sheet to adapt the 3-columns layout

--- modules/mod_property_adsense_side/mod_property_adsense_side.php
+++ modules/mod_property_adsense_side/mod_property_adsense_side.php
@@ -1,7 +1,10 @@
 <div>
+    <script type="text/javascript" >
 	<?php 
-
 		$code = $params->get("code");
 		echo $code ;
 	?>
+    </script>
+
+    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
 </div>
